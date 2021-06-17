ad3be214243c24dd3dd07764e1bf84b2861b7354
Subhendu Paul <subhendup@markit-systems.com>
2021-06-17 08:12:55 +0000

    UAT update on 17-06-2021 13:42

--- src/component/health/ThankYou.jsx
+++ src/component/health/ThankYou.jsx
@@ -17,7 +17,7 @@ class ThankYouPage extends Component {
   state = {
     accessToken: "",
     response_text: [],
-    policyNo: this.props.match.params,
+    policyNo: this.props.match.params.policyId,
     vehicletype: [],
     dloadCounter: 0,
     refNumber:  queryString.parse(this.props.location.search).access_id ? 
